5e05a9e87a7d73acf54c3017584569ec5e94420f
Andressa Tonietto Reis <andressatreis@gmail.com>
2022-06-01 15:04:15 +0200

    add search and tv shows list

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,36 @@
+import { useState } from 'react';
+import Card from '../../components/Card';
+import Link from '../../components/Link';
+import Search from '../../components/Search';
+
 const Home = () => {
-  return <div>Home</div>;
+  const [shows, setShows] = useState<[TVShowInfo] | []>([]);
+
+  return (
+    <div>
+      <nav
+        style={{
+          borderBottom: 'solid 1px',
+          paddingBottom: '1rem',
+          display: 'flex',
+        }}
+      >
+        <Link to="/">TV Series Search</Link>
+        <Search setShows={setShows} />
+      </nav>
+      <div>
+        {shows?.map(show => (
+          <Card
+            key={show.show.id}
+            id={show.show.id}
+            name={show.show.name}
+            image={show.show.image?.original}
+            rating={show.show.rating.average}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
