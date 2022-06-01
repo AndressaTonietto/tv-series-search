9c9305982b27b0ca8e1d25a287d1cb5134d53c6e
Andressa Tonietto Reis <andressatreis@gmail.com>
2022-06-01 18:26:12 +0200

    add style to card and link

--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -1,9 +1,17 @@
 import styled from 'styled-components';
+import { cardWidth } from '../../config';
 
 export const StyledCard = styled.div`
-  width: 180px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: ${cardWidth}px;
+  background-color: #161b22;
+  border-radius: 6px;
 `;
+
 export const Footer = styled.div`
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
+  padding: 0 5px;
 `;
--- src/components/Link/styles.js
+++ src/components/Link/styles.js
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const StyledLink = styled(Link)`
-  text-decoration: none;
+  text-decoration: inherit;
+  color: inherit;
   height: 100%;
 `;
