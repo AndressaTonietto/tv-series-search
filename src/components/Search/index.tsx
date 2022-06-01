f7046d8579e98f1c3f8b0d00417f3cd919d87a64
Andressa Tonietto Reis <andressatreis@gmail.com>
2022-06-01 14:53:49 +0200

    ✨ add Search component

---
+++ src/components/Search/index.tsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import api from '../../services/api';
+
+const Search = ({
+  setShows,
+}: {
+  setShows: React.Dispatch<React.SetStateAction<[TVShowInfo] | []>>;
+}) => {
+  const [search, setSearch] = useState('');
+
+  const handleSearch = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      const url = `search/shows?q=${search}`;
+      const res = await api.get(url);
+      setShows(res.data);
+    } catch (e) {
+      console.log(e);
+    }
+    clearInput();
+  };
+
+  function clearInput() {
+    setSearch('');
+  }
+
+  return (
+    <form onSubmit={event => handleSearch(event)}>
+      <input
+        type="text"
+        id="search"
+        value={search}
+        onChange={e => setSearch(e.target.value)}
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+};
+
+export default Search;
