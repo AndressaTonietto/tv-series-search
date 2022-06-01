f27839cd45c3153d06ab00cf3ede4ae426e41074
Andressa Tonietto Reis <andressatreis@gmail.com>
2022-06-01 10:44:30 +0200

    fix api base url

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://api.tvmaze.com/search/shows?q=',
+  baseURL: 'https://api.tvmaze.com/search/shows',
 });
 
 export default api;
