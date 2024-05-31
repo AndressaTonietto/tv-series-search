bad9f4126d69fc762182c8096032d3e9614df6af
Andressa <andressatreis@gmail.com>
2024-05-31 09:55:12 +0200

    add errorPage

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import Details from "./routes/details.tsx";
+import ErrorPage from "./components/errorPage.tsx";
 import Home from "./routes/home.tsx";
 import React from "react";
 import ReactDOM from "react-dom/client";
@@ -11,6 +12,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+    errorElement: <ErrorPage />,
   },
   {
     path: "/details",
