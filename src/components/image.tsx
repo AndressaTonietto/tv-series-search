d131bcd90d1a8280f89d5f81b66d653fb4ecd79e
Andressa <andressatreis@gmail.com>
2024-06-02 15:58:32 +0200

    remove unused prop

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -1,12 +1,10 @@
 interface ImageProps {
   alt: string;
   src: string | undefined;
-  className?: string;
 }
 
-const Image = ({ alt, src, className }: ImageProps) => {
-  if (src)
-    return <img alt={alt} src={src} className={`max-w-52 ${className}`} />;
+const Image = ({ alt, src }: ImageProps) => {
+  if (src) return <img alt={alt} src={src} className="max-w-52" />;
   return (
     <div className="aspect-[2/3] justify-center max-w-52 flex items-center px-2 badge-neutral">
       No image available
